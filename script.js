be7e716a0a651f3a0989c08c3c7141c450f6e08e
barmakow <barbaramakowski@gmail.com>
2024-04-30 15:09:58 -0700

    home page: slides, title, short description, boxes to link to other pages

---
+++ script.js
@@ -0,0 +1,25 @@
+// Interactive boxes
+function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+
+// Slideshow
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
