ffe15b194521fd5eb53f80ecfc1f80477d3ac264
Michael Moore <bazcrownn@gmail.com>
2024-05-19 15:33:45 -0700

    updated activity, activityPuller and css

--- scripts/activityPuller.js
+++ scripts/activityPuller.js
@@ -8,6 +8,14 @@ function getParameterByName(url, name) {
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
+
+function getFileNameFromUrl(url) {
+  const urlParts = url.split('/');
+  const fileName = urlParts[urlParts.length - 1];
+
+  return fileName.replace(/\s+/g, '-');
+}
+
 const currentUrl = window.location.href;
 const pulledId = getParameterByName(currentUrl, 'id');
 
@@ -15,10 +23,34 @@ let xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://j9as09z9sd.execute-api.us-west-2.amazonaws.com/prod/seattle?id=' + pulledId, true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 
+let activityName = document.getElementById("activityName");
+let activityImage = document.getElementById("activityImage");
+let activityLocation = document.getElementById("activityLocation");
+
+
 xhr.onload = function() {
     if (xhr.status === 200) {
       let responseData = JSON.parse(xhr.responseText);
+      let jsonData = JSON.parse(responseData.Item.jsonData);
       console.log(responseData);
+      console.log(jsonData.name);
+
+      activityName.innerHTML = `<h1>${jsonData.name}</h1>`;
+
+
+      
+      const ItemImage = document.createElement('img');
+      urlPath = getFileNameFromUrl(jsonData.photo.images.medium.url);
+      ItemImage.src = "images/" + urlPath;
+      ItemImage.alt = jsonData.photo.caption;
+
+      activityImage.appendChild(ItemImage);
+
+      activityLocation.innerHTML = `<h1>${jsonData.name}</h1>`;
+      
+
+    }else{
+      console.error('Request failed. Status:', xhr.status);
     }
 };
 
